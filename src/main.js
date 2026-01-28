9043489040aa3c1773c723d73a38254255969412
tigranspixeria <tigran.s.pixeria@gmail.com>
2026-01-28 23:52:49 +0400

    feat: setup Three.js scene, camera and renderer

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
 import './style.css';
+import * as THREE from 'three';
 
-console.log('Mini Solar System');
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.z = 10;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#scene'),
+    antialias: true,
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+// Render once
+renderer.render(scene, camera);
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
